db7bcbf0a2fa33eddcbe0d2841256fe1394fda09
taka16-tnk <taka16.tanaka@gmail.com>
2021-12-12 16:34:51 +0900

    Soket.io Events

--- src/index.js
+++ src/index.js
@@ -4,17 +4,29 @@ const app = express();
 const http = require('http');
 const socketio = require('socket.io');
 
-const port = process.env.PORT || 3000
-const publicDirectoryPath = path.join(__dirname, '../public');
 const server = http.createServer(app)
 const io = socketio(server)
 
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public');
+
 app.use(express.static(publicDirectoryPath));
+
+let count = 0;
+
+io.on('connection', (socket) => {
+  console.log('New WebSocket connection')
+
+  socket.emit('countUpdated', count);
+
+  socket.on('increment', () => {
+    count++;
+    // socket.emit('countUpdated', count);
+    io.emit('countUpdated', count); // リアルタイム更新となる
+  });
+});
+
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
 });
-
-io.on('connection', () => {
-  console.log('New WebSocket connection')
-})
